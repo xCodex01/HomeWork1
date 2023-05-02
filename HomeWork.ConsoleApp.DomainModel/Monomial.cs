c83c1a82b728a8f13620a94c50dff70b0dc1449c
xCodex01 <francy012001@gmail.com>
2023-05-02 17:11:40 +0200

    g

--- HomeWork.ConsoleApp.DomainModel/Monomial.cs
+++ HomeWork.ConsoleApp.DomainModel/Monomial.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HomeWork.ConsoleApp.DomainModel
+﻿namespace HomeWork.ConsoleApp.DomainModel
 {
     public partial class Monomial
     {
--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,10 +1,4 @@
 ﻿using HomeWork.ConsoleApp.DomainModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork1.ConsoleApp.App
 {
